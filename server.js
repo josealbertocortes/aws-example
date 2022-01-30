65b61909808aa1aaf23eea47633429d84fdba5d9
josealbertocortes <josealbertocortesayala94@gmail.com>
2022-01-29 19:40:22 -0600

    respondiendo con html

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 
 const app = express()
 
-app.get(('/'),(req,res)=>res.send("hello worlf with express"))
+app.get(('/'),(req,res)=>res.send("<h1>hola con html </h1>"))
 
 
 app.listen(3000)
